71bdcb008b685085d0347ba8503561c2efcff41a
henrywang08 <henry_ly@hotmail.com>
2025-02-20 10:54:13 +0800

    Update CheckWhoRegisterdApp.ps1

--- CheckWhoRegisterdApp.ps1
+++ CheckWhoRegisterdApp.ps1
@@ -1,6 +1,24 @@
-Import-Module Microsoft.Graph.Authentication
+# Install the Microsoft.Graph module if not installed
 
-Connect-MgGraph -Scopes "AuditLog.Read.All", "Directory.Read.All" -ContextScope Process
+$RequiredModules = @("Microsoft.Graph.Authentication", "Microsoft.Graph.Applications", "Microsoft.Graph.Reports")
+foreach ($module in $RequiredModules) {
+    if (-not (Get-Module -ListAvailable -Name $module)) {
+        Install-Module -Name $module -Force -AllowClobber
+    }
+}
+
+# Import the required modules
+foreach ($module in $RequiredModules) {
+    Import-Module $module
+}
+
+Connect-MgGraph -Scopes "AuditLog.Read.All", "Directory.Read.All" 
+
+# Check if you're authenticated by attempting to retrieve a user object
+$context = Get-MgContext
+
+# If you're authenticated, this should return user details without error
+Write-Host "Authenticated as: $($context.Account)"
 
 # Query the audit logs for app creation events:
 
@@ -12,3 +30,4 @@ $appAudit | Select-Object @{N='AppName';E={$_.TargetResources[0].DisplayName}}, 
             @{N='UserPrincipalName';E={$_.InitiatedBy.User.UserPrincipalName}}, `
             @{N='DisplayName';E={$_.InitiatedBy.User.DisplayName}}
 
+Disconnect-MgGraph
